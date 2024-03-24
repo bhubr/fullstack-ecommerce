811de8816386c5b9e87b872a3944357f853f0524
Benoît Hubert <benoithubert@gmail.com>
2024-03-24 20:57:51 +0100

    Rework order submit form

--- frontend/src/components/CheckoutForm.tsx
+++ frontend/src/components/CheckoutForm.tsx
@@ -32,102 +32,112 @@ const CheckoutForm = () => {
   };
 
   return (
-    <Container>
-      <Row>
-        <Col xs={{ offset: 2, size: 8 }}>
-          <Form onSubmit={handleSubmit}>
-            <FormGroup>
-              <Label for="address">Adresse</Label>
-              <Input
-                type="text"
-                name="address"
-                id="address"
-                value={formData.address}
-                onChange={handleChange}
-              />
-            </FormGroup>
-            <Row>
-              <Col xs="4">
-                <FormGroup>
-                  <Label for="postalCode">Code Postal</Label>
-                  <Input
-                    type="text"
-                    name="postalCode"
-                    id="postalCode"
-                    value={formData.postalCode}
-                    onChange={handleChange}
-                  />
-                </FormGroup>
-              </Col>
-              <Col xs="8">
-                <FormGroup>
-                  <Label for="city">Ville</Label>
-                  <Input
-                    type="text"
-                    name="city"
-                    id="city"
-                    value={formData.city}
-                    onChange={handleChange}
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-            <FormGroup>
-              <Label for="phone">Téléphone</Label>
-              <Input
-                type="text"
-                name="phone"
-                id="phone"
-                value={formData.phone}
-                onChange={handleChange}
-              />
-            </FormGroup>
-            <hr style={{ margin: '3em 0' }} />
-            <Row>
-              <Col xs="6">
-                <FormGroup>
-                  <Label for="cardNumber">Numéro de Carte de Crédit</Label>
-                  <Input
-                    type="text"
-                    name="cardNumber"
-                    id="cardNumber"
-                    value={formData.cardNumber}
-                    onChange={handleChange}
-                  />
-                </FormGroup>
-              </Col>
-              <Col xs="3">
-                <FormGroup>
-                  <Label for="cvv">CVV</Label>
-                  <Input
-                    type="text"
-                    name="cvv"
-                    id="cvv"
-                    value={formData.cvv}
-                    onChange={handleChange}
-                  />
-                </FormGroup>
-              </Col>
-              <Col xs="3">
-                <FormGroup>
-                  <Label for="expiration">Date d'expiration</Label>
-                  <Input
-                    type="text"
-                    name="expiration"
-                    id="expiration"
-                    value={formData.expiration}
-                    onChange={handleChange}
-                  />
-                </FormGroup>
-              </Col>
-            </Row>
-            <Button color="primary" type="submit">
-              Finaliser la Commande
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+    <Row>
+      <Col>
+        <h4>Informations de Livraison</h4>
+        <Form onSubmit={handleSubmit}>
+          <FormGroup>
+            <Label for="address">Adresse</Label>
+            <Input
+              type="text"
+              name="address"
+              id="address"
+              value={formData.address}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <Row>
+            <Col xs="4">
+              <FormGroup>
+                <Label for="postalCode">Code Postal</Label>
+                <Input
+                  type="text"
+                  name="postalCode"
+                  id="postalCode"
+                  value={formData.postalCode}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs="8">
+              <FormGroup>
+                <Label for="city">Ville</Label>
+                <Input
+                  type="text"
+                  name="city"
+                  id="city"
+                  value={formData.city}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <FormGroup>
+            <Label for="phone">Téléphone</Label>
+            <Input
+              type="text"
+              name="phone"
+              id="phone"
+              value={formData.phone}
+              onChange={handleChange}
+            />
+          </FormGroup>
+          <Row className="my-4">
+            <Col>
+              <h4>Informations de Paiement</h4>
+              <p className="text-danger fst-italic" style={{ fontSize: 14 }}>
+                Évidemment, ne renseignez pas de vraies informations de
+                paiement&hellip;
+                <br />
+                (elles ne seront pas stockées, de toute façon)
+              </p>
+            </Col>
+            <Col xs="12">
+              <FormGroup>
+                <Label for="cardNumber">Numéro de Carte de Crédit</Label>
+                <Input
+                  type="text"
+                  name="cardNumber"
+                  id="cardNumber"
+                  placeholder="1234 5678 9012 3456"
+                  value={formData.cardNumber}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs="6">
+              <FormGroup>
+                <Label for="cvv">CVV</Label>
+                <Input
+                  type="text"
+                  name="cvv"
+                  id="cvv"
+                  placeholder="123"
+                  value={formData.cvv}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col xs="6">
+              <FormGroup>
+                <Label for="expiration">Date d'expiration</Label>
+                <Input
+                  type="text"
+                  name="expiration"
+                  id="expiration"
+                  placeholder="12/26"
+                  value={formData.expiration}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Button color="primary" type="submit">
+            Finaliser la Commande
+          </Button>
+        </Form>
+      </Col>
+    </Row>
   );
 };
 
--- frontend/src/pages/SubmitOrder.tsx
+++ frontend/src/pages/SubmitOrder.tsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { Button, Container, Col, Row } from 'reactstrap';
+import { useContext, useMemo } from 'react';
+import { Container, Col, Row } from 'reactstrap';
 
 import type { ICartItem } from '../types';
 import { serverUrl } from '../settings';
@@ -7,10 +7,12 @@ import CartContext from '../contexts/CartContext';
 import { formatPrice } from '../helpers';
 import CheckoutForm from '../components/CheckoutForm';
 
+import './SubmitOrder.css';
+
 const OrderItem = ({ item }: { item: ICartItem }) => {
   return (
     <Row>
-      <Col xs={{ offset: 2, size: 2 }} className="d-flex align-items-center">
+      <Col xs={{ size: 3 }} className="d-flex align-items-center">
         <div>
           <img
             src={`${serverUrl}${item.product.pictureUrl}`}
@@ -19,9 +21,9 @@ const OrderItem = ({ item }: { item: ICartItem }) => {
           />
         </div>
       </Col>
-      <Col xs="6">
-        <div className="py-5">
-          <p className="fs-6">{item.product.name}</p>
+      <Col xs="9">
+        <div className="py-3">
+          <p style={{ fontSize: 15 }}>{item.product.name}</p>
           {item.quantity} x {formatPrice(item.product.price)}
         </div>
       </Col>
@@ -29,37 +31,46 @@ const OrderItem = ({ item }: { item: ICartItem }) => {
   );
 };
 
-const Total = ({ items }: { items: ICartItem[] }) => (
-  <>
-    {formatPrice(
-      items.reduce(
-        (total, item) => total + item.product.price * item.quantity,
-        0
-      )
-    )}
-  </>
+const SummaryItem = ({ label, price }: { label: string; price: number }) => (
+  <li>
+    <span className="SubmitOrder-label">{label}</span>
+    <span className="SubmitOrder-price">{formatPrice(price)}</span>
+  </li>
 );
 
 const SubmitOrder = () => {
   const { items } = useContext(CartContext);
+  const subTotal = useMemo(
+    () =>
+      items.reduce(
+        (total, item) => total + item.product.price * item.quantity,
+        0
+      ),
+    [items]
+  );
 
   return (
     <Container className="my-5">
       <h1 className="flex-grow-1">Finaliser la commande</h1>
-      {items.map((item) => (
-        <OrderItem key={item.product.id} item={item} />
-      ))}
       <Row className="my-2">
-        <Col xs={{ offset: 4, size: 8 }}>
-          <h2>Récapitulatif</h2>
-          <div className="my-2">
-            <strong>
-              Total : <Total items={items} />
-            </strong>
+        <Col md="5">
+          <div className="my-4">
+            <h4>Récapitulatif</h4>
+            <ul>
+              <SummaryItem label="Sous-total" price={subTotal} />
+              <SummaryItem label="Livraison" price={0} />
+              <SummaryItem label="Total" price={subTotal} />
+            </ul>
           </div>
+          <CheckoutForm />
+        </Col>
+        <Col md={{ offset: 1, size: 6 }}>
+          <h4>Produits</h4>
+          {items.map((item) => (
+            <OrderItem key={item.product.id} item={item} />
+          ))}
         </Col>
       </Row>
-      <CheckoutForm />
     </Container>
   );
 };
