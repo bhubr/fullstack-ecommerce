a2ac9ac8fdcd6536597407e667c438f160305363
Benoît Hubert <benoithubert@gmail.com>
2024-03-24 19:11:55 +0100

    Fix password generation in create-user CLI tool

--- backend/tools/create-user.ts
+++ backend/tools/create-user.ts
@@ -7,35 +7,49 @@ if (email === undefined) {
   console.error('usage: ts-node create-user.ts <email>');
   process.exit(1);
 }
-let passwordLen = Number(process.argv[3])
+let passwordLen = Number(process.argv[3]);
 if (Number.isNaN(passwordLen)) {
   passwordLen = 8;
 }
 
+// randomly determine "range" (digit, upper, etc.) that we're going to pick from
+const pickRangeType = (): string => {
+  const byte = crypto.randomBytes(1)[0];
+  if (byte < 56) {
+    return 'digit';
+  } else if (byte < 112) {
+    return 'upper';
+  } else if (byte < 168) {
+    return 'lower';
+  } else if (byte < 190) {
+    return 'special1';
+  } else if (byte < 212) {
+    return 'special2';
+  } else if (byte < 234) {
+    return 'special3';
+  } else {
+    return 'special4';
+  }
+};
+
 // simple password generator
-const generatePassword = (len = 8): string => {
+const generatePassword = (len = 10): string => {
   const ranges: Record<string, [number, number]> = {
-    digit: [48, 57],
-    upper: [65, 90],
-    lower: [97, 122],
-    special1: [33, 47],
-    special2: [58, 64],
-    special3: [91, 96],
-    special4: [123, 126],
+    digit: [48, 57], // range of 10 digits
+    upper: [65, 90], // range of 26 uppercase letters
+    lower: [97, 122], // range of 26 lowercase letters
+    special1: [33, 47], // range of 15 special characters
+    special2: [58, 64], // range of 7 special characters
+    special3: [91, 96], // range of 6 special characters
+    special4: [123, 126], // range of 4 special characters
   };
   const bytes = Array.from(crypto.randomBytes(len));
   return bytes
     .map((byte: number): string => {
-      for (const [, [min, max]] of Object.entries(ranges)) {
-        if (byte >= min && byte <= max) {
-          return String.fromCharCode(byte);
-        }
-      }
-      // default: pick any from special{1..4}
-      const modulo = byte % 16;
-      return modulo < 15
-        ? String.fromCharCode(33 + modulo)
-        : String.fromCharCode(126);
+      const rangeType = pickRangeType();
+      const [min, max] = ranges[rangeType];
+      // compute value within the range, basically byte modulo (max - min + 1) + min
+      return String.fromCharCode(min + (byte % (max - min + 1)));
     })
     .join('');
 };
