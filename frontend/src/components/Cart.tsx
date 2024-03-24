299ba506646018fe44caa27ceb0129884eb0ad4a
Benoît Hubert <benoithubert@gmail.com>
2024-03-24 15:05:56 +0100

    Allow to modify qty + order btn

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,13 +1,34 @@
 import { useContext } from 'react';
-import { Container, Col, Row } from 'reactstrap';
+import { Button, Container, Col, Row } from 'reactstrap';
 
 import type { ICartItem } from '../types';
 import { serverUrl } from '../settings';
 import CartContext from '../contexts/CartContext';
 import { formatPrice } from '../helpers';
 
-const CartItem = ({ item }: { item: ICartItem }) => (
-  <Container>
+interface IQuantityDropdownProps {
+  quantity: number;
+  onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+}
+
+const QuantityDropdown = ({ quantity, onChange }: IQuantityDropdownProps) => (
+  <select
+    value={quantity}
+    className="form-select"
+    onChange={onChange}
+    style={{ display: 'inline', width: '70px' }}
+  >
+    {[...Array(10)].map((_, i) => (
+      <option key={i} value={i + 1}>
+        {i + 1}
+      </option>
+    ))}
+  </select>
+);
+
+const CartItem = ({ item }: { item: ICartItem }) => {
+  const { removeItem, setItemQuantity } = useContext(CartContext);
+  return (
     <Row>
       <Col xs="4">
         <img
@@ -19,24 +40,75 @@ const CartItem = ({ item }: { item: ICartItem }) => (
       <Col xs="8">
         <div className="py-5">
           <p>{item.product.name}</p>
+          <p>{item.product.description}</p>
           <p>
-            {item.quantity} x {formatPrice(item.product.price)}
+            Quantité :{' '}
+            <QuantityDropdown
+              quantity={item.quantity}
+              onChange={(e) =>
+                setItemQuantity(item.product, Number(e.target.value))
+              }
+            />{' '}
+            x {formatPrice(item.product.price)}
+            <Button
+              outline
+              color="danger"
+              className="mx-3"
+              onClick={() => removeItem(item.product)}
+            >
+              Supprimer
+            </Button>
           </p>
         </div>
       </Col>
     </Row>
-  </Container>
+  );
+};
+
+const Total = ({ items }: { items: ICartItem[] }) => (
+  <>
+    {formatPrice(
+      items.reduce(
+        (total, item) => total + item.product.price * item.quantity,
+        0
+      )
+    )}
+  </>
 );
 
 const Cart = () => {
   const { items } = useContext(CartContext);
 
   return (
-    <div>
+    <Container className="my-5">
+      <div className="d-flex d-justify-between">
+        <h1 className="flex-grow-1">Panier</h1>
+        <div className="me-auto">
+          <span className="mx-2">
+            <Total items={items} />
+          </span>
+          <Button size="sm" color="primary" className="me-2">
+            Commander
+          </Button>
+        </div>
+      </div>
       {items.map((item) => (
         <CartItem key={item.product.id} item={item} />
       ))}
-    </div>
+      <Row className="my-2">
+        <Col xs={{ offset: 4, size: 8 }}>
+          <h2>Récapitulatif</h2>
+          <div className="my-2">
+            <strong>
+              Total : <Total items={items} />
+            </strong>
+          </div>
+          <Button size="lg" color="primary" className="me-2">
+            Commander
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
