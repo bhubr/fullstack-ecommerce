b9983c4e70a49087b93a4f6297a7c3259882da1f
Benoît Hubert <benoithubert@gmail.com>
2024-03-26 00:54:40 +0100

    Fix flaky signup test by using a more specific dropdown selector

--- test/specs/signup.e2e.ts
+++ test/specs/signup.e2e.ts
@@ -3,17 +3,29 @@ import { registerUser } from "../register-user.ts";
 
 describe("Register", () => {
   it("should register with valid information", async () => {
-    await registerUser("John Doe", "johndoe@" + Date.now() + "example.com", "Abcd1234!");
+    await registerUser(
+      "John Doe",
+      "johndoe@" + Date.now() + "example.com",
+      "Abcd1234!"
+    );
 
-    await browser.pause(5000);
+    // await browser.pause(1000);
 
-    const firstDropdownToggle = await $(".dropdown-toggle");
-    const firstDropdownToggleText = await firstDropdownToggle.getText();
-    expect(firstDropdownToggleText).toContain("John Doe");
+    // Utiliser le sélecteur '.dropdown' n'est pas fiable ici, car entre les modes invité et authentifié,
+    // on a un dropdown au même endroit, mais avec un contenu différent.
+    // Ici on utilise un sélecteur plus spécifique pour le dropdown "authentifié".
+    const authenticatedDropdown = await $("#authenticated-dropdown");
+    await authenticatedDropdown.waitForExist();
+    const dropdownText = await authenticatedDropdown.getText();
+    expect(dropdownText).toContain("John Doe");
 
     // Déconnexion après le test
-    await firstDropdownToggle.click();
-    const dropdownButtons = await $$(".dropdown .dropdown-menu button");
+    // Récupération du bouton "toggle" pour ouvrir le dropdown
+    const authenticatedDropdownToggle = await authenticatedDropdown.$(".dropdown-toggle");
+    await authenticatedDropdownToggle.click();
+    // Récupération de la liste des boutons dans le menu du dropdown
+    const dropdownButtons = await authenticatedDropdown.$$(".dropdown-menu button");
+    // Le bouton de déconnexion est le dernier de la liste
     const logoutButton = dropdownButtons[dropdownButtons.length - 1];
     await logoutButton.click();
   });
@@ -21,10 +33,11 @@ describe("Register", () => {
   it("should not register with invalid information", async () => {
     await registerUser("John Doe", "", "Abcd1234!");
 
-    $(".alert-danger").waitForExist({ timeout: 2000 });
-
-    const alertDanger = await $(".alert-danger");
-    const alertDangerText = await alertDanger.getText();
-    expect(alertDangerText).toContain("L'e-mail et le mot de passe sont requis");
+    const alert = await $(".alert-danger");
+    await alert.waitForExist();
+    const alertText = await alert.getText();
+    expect(alertText).toContain(
+      "L'e-mail et le mot de passe sont requis"
+    );
   });
 });
