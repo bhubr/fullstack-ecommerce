e6d3f61221bf26c2115927052d8b3ad3a3331322
Benoît Hubert <benoithubert@gmail.com>
2024-03-26 09:14:35 +0100

    Fix register test - happy path

--- test/specs/signup.e2e.ts
+++ test/specs/signup.e2e.ts
@@ -4,21 +4,33 @@ describe("Register", () => {
   it("should register with valid information", async () => {
     await browser.url(`http://localhost:5173/compte/inscription`);
 
+    const inputFullName = await $("#inputFullName");
+    await inputFullName.waitForDisplayed();
+
     await $("#inputFullName").setValue("John Doe");
     await $("#inputEmail").setValue("johndoe@" + Date.now() + "example.com");
     await $("#inputPassword").setValue("Abcd1234!");
     await $('button[type="submit"]').click();
 
-    await browser.pause(5000);
+    // On utilise l'id plutôt que la classe .dropdown
+    // pour être sûr de récupérer le dropdown authentifié et pas le "invité"
+    const authenticatedDropdown = await $("#authenticated-dropdown");
+    await authenticatedDropdown.waitForDisplayed();
 
-    const firstDropdownToggle = await $(".dropdown-toggle");
-    const firstDropdownToggleText = await firstDropdownToggle.getText();
-    expect(firstDropdownToggleText).toContain("John Doe");
+    // On récupère le .dropdown-toggle, pas n'importe lequel,
+    // mais celui à l'intérieur de authenticatedDropdown
+    const authenticatedDropdownToggle = await authenticatedDropdown.$(
+      ".dropdown-toggle"
+    );
+    const toggleText = await authenticatedDropdownToggle.getText();
+    expect(toggleText).toContain("John Doe");
 
     // Déconnexion après le test
-    await firstDropdownToggle.click();
-    const dropdownButtons = await $$(".dropdown .dropdown-menu button");
-    const logoutButton = dropdownButtons[dropdownButtons.length - 1];
+    // Permet d'ouvrir le dropdown
+    await authenticatedDropdownToggle.click();
+    // const dropdownButtons = await $$(".dropdown .dropdown-menu button");
+    // const logoutButton = dropdownButtons[dropdownButtons.length - 1];
+    const logoutButton = await $(".signout-btn");
     await logoutButton.click();
   });
 
@@ -34,6 +46,8 @@ describe("Register", () => {
 
     const alertDanger = await $(".alert-danger");
     const alertDangerText = await alertDanger.getText();
-    expect(alertDangerText).toContain("L'e-mail et le mot de passe sont requis");
+    expect(alertDangerText).toContain(
+      "L'e-mail et le mot de passe sont requis"
+    );
   });
 });
